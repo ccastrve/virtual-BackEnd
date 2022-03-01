027fc2bcfed0f571336e29d670198aa3b3902e22
ccastrve <cristhian.castro.veliz1@gmail.com>
2022-02-28 21:31:16 -0500

    declaracion primeros archivos

---
+++ dia1/02-impresion-pantalla.py
@@ -0,0 +1,22 @@
+nombre = 'Cristhian'
+estado_civil = 'Casado'
+#la coma te agrega un espacio
+
+print('Mi nombre es: ', nombre, ', soy el mejor')
+
+print('Mi nombre es {} y soy {}'.format(nombre, estado_civil))
+
+print('soy {1} mi nombre es {0}'.format(nombre, estado_civil))
+
+
+
+
+
+
+
+
+
+
+
+
+
